7ac063ac6e8abc16a005721df65934c7f2103908
kinga00099 <kinga00099@gmail.com>
2018-10-14 22:21:36 +0200

    scanner is only needed once

--- src/com/codecool/tinker/Main.java
+++ src/com/codecool/tinker/Main.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Main {
     static void printResults() {
         Scanner size = new Scanner(System.in);
-        Scanner index = new Scanner(System.in);
         System.out.println("Please enter the number of elements: ");
         int n = size.nextInt();
         System.out.println("Please enter the constant: ");
-        int k = index.nextInt();
+        int k = size.nextInt();
         Tinker winner = new Tinker();
         System.out.println("The winner of the game n = " + n + ", k = " + k + " is " + winner.calculateWinner(n, k));
     }
